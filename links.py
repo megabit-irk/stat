d2abaf93c577b21820d3aec7c16fb5cd6f1df9b1
metaIhead <vipkingston1000@mail.ru>
2019-06-11 16:18:57 +0800

    11.06.2019 16:20

---
+++ links.py
@@ -0,0 +1,1 @@
+link1='https://megabit38.ru:9443/admin/index.cgi?get_index=dv_users_list&EXPORT_CONTENT=INTERNET_USERS_LIST&header=1&json=1&PAGE_ROWS=1000000&API_KEY=9954782216weDHUSdhgsdGJ544'
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import requests
-r = requests.get('https://megabit38.ru:9443/admin/index.cgi?get_index=dv_users_list&EXPORT_CONTENT=INTERNET_USERS_LIST&header=1&json=1&PAGE_ROWS=1000000&API_KEY=9954782216weDHUSdhgsdGJ544', verify=False)
-print(r.text[1])
+import json
+import links
+
+r = requests.get(links.link1, verify=False)
+jason_file=r.json()
+keys=jason_file.keys()
+print(keys)
+
+print(len(jason_file['DATA_1'][0]))
+print(type(jason_file['DATA_1'][0]))
+print(jason_file['DATA_1'][0])
+
+login=
