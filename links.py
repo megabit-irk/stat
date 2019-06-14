2e8ba50958ee4b7d39cfea9566de3daa0789df3f
metaIhead <vipkingston1000@mail.ru>
2019-06-14 17:46:11 +0800

    UID

--- links.py
+++ links.py
@@ -1,1 +1,2 @@
 link1='https://megabit38.ru:9443/admin/index.cgi?get_index=dv_users_list&EXPORT_CONTENT=INTERNET_USERS_LIST&header=1&json=1&PAGE_ROWS=1000000&API_KEY=9954782216weDHUSdhgsdGJ544'
+link2='https://megabit38.ru:9443/admin/index.cgi?qindex=7&search=1&type=10&header=1&json=1&EXPORT_CONTENT=USERS_LIST&SKIP_FULL_INFO=1&PAGE_ROWS=412&API_KEY=9954782216weDHUSdhgsdGJ544'
--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import requests
+import cjson
 import json
 import links
 
-r = requests.get(links.link1, verify=False)
-jason_file=r.json()
-keys=jason_file.keys()
+r = requests.get(links.link2, verify=False)
+json_file=r.json()
+keys=json_file.keys()
+
 print(keys)
 
-print(len(jason_file['DATA_1'][0]))
-print(type(jason_file['DATA_1'][0]))
-print(jason_file['DATA_1'][0])
+#print(json_file)
 
-login=
+# print(type(json_file['DATA_1']))
+# print(json_file['DATA_1'][0])
+# print(type(json_file['DATA_1'][0]['login']))
+#
+# q_users=len(json_file['DATA_1'])
+#
+# for index in range(0,q_users):
+#     login=json_file['DATA_1'][index]['login']
+#     name=json_file['DATA_1'][index]['fio']
+#     # date_on=json_file['DATA_1'][index]['login']
+#     # date_off=json_file['DATA_1'][index]['login']
+#     credit=json_file['DATA_1'][index]['credit']
+#     deposit=json_file['DATA_1'][index]['deposit']
